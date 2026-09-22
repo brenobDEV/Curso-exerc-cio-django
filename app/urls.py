09c6a218124a37763cbebc1d72a0755c18e14b14
brenobDEV <brenobraga411@gmail.com>
2026-09-21 21:50:12 -0300

    aula 9

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home_view, name='home'),
     path('perfil/', views.perfil_view, name='perfil'),
-    path('status/', views.Status_view, name='status')
+    path('status/', views.status_view, name='status'),
+    path('produtos/', views.produtos_view, name='produtos'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,24 +1,29 @@
 from django.shortcuts import render
 
+
 def home_view(request):
-    nome_empresa{
-        ["Shopeeamazo_nome_legal"]
-    }
+    return render(request,'home.html')
 
-    return render(request, 'home.html')
+def produtos_view(request):
+    
+    lista_produtos = [
+            {'nome': "Monitor", "preco": 700.00, "estoque": 5},
+            {'nome': "PC Desktop", "preco": 4500.00, "estoque": 2},
+            {'nome': "SmartWach", "preco": 200.00, "estoque": 7},
+            {'nome': "Cadeira Gamer", "preco": 900.00, "estoque": 1},
+            {'nome': "Teclado Mecânico", "preco": 400.00, "estoque": 11},
+    ]
+    
+    context = {'produtos': lista_produtos}
 
+    return render(request,'produtos.html', context)
 
 def perfil_view(request):
-    context{
-        ['nome_funcionario': "haroldo", 'cargo': "Dev front-end", 'setor': "finança"]
-    }
-     
-     return render(request, 'perfil.html', context)
+    context = {'nome_usuario': 'Gustavo' , 'cargo': 'Instrutor' , 'setor': 'TI'}
 
+    return render(request,'perfil.html', context)
 
 def status_view(request):
-    context{
-        ['id_servidor': "Servidor Alpha-01", 'status_sistema': "Operacional"]
-    }
+    context = {'admin': True ,'id_servidor': '127.0.0.1' , 'status_sistema': '200 OK - Online'}
 
-    return render(request, 'Status.html', context)
+    return render(request,'status.html', context)
