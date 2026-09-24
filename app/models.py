0cf9c1becb6dcbd08bfd71b08cd2970b758ce7ae
brenobDEV <brenobraga411@gmail.com>
2026-09-23 21:50:27 -0300

    Aula10

--- app/models.py
+++ app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+
+# tipo string -> CharField(precisa do tamanho máximo)
+# tipo inteiro -> IntegerField()
+# tipo decimal -> FloatField()
+# tipo booleano -> BooleanField()
+
+class Produto(models.Model): #O CERTO É PRODUTO NO SINGULAR
+    nome = models.CharField(max_length=200)
+    preco = models.FloatField()
+    estoque = models.IntegerField()
+    imagem = models.CharField(max_length=500, default='')   
+
+    def __str__(self):
+        return self.nome
